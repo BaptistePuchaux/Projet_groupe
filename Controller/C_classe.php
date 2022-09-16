d4db429e3f61916d46a3312901ff056149749d16
JELLOUL <JELLOUL@D9.bts.leo>
2022-09-16 11:01:55 +0200

    Connexion

---
+++ Controller/C_classe.php
@@ -0,0 +1,7 @@
+<?php
+        require '../Modele/Connexion_bd.php';
+        require '../Vue/Sommaire.php';
+        $classe = recupere_classes();
+        
+        require_once '../Vue/Classe.php';
+        ?>
